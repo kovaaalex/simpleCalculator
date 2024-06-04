3ae03a1710cad140d37afc39a09ecf6e1e14ac15
kovaaalex <kovalenko.alex04@gmail.com>
2024-06-05 01:08:43 +0300

    twelve

---
+++ addValutes.js
@@ -0,0 +1,5 @@
+const from = document.getElementById('from')
+from.innerHTML +=
+`<div class="country">
+    Spain
+</div>`
--- financeCalculator.js
+++ financeCalculator.js
@@ -1,20 +1,39 @@
     const finNumbers = document.querySelectorAll('.finance .basic__digi')
     const amountDisplay = document.getElementById('amount')
+    const finance__backspace = document.getElementById('finance__backspace')
+    const finance__clear = document.getElementById('finance__clear')
     finNumbers.forEach(nm => nm.addEventListener('click', () => addNmbr(nm.outerText)))
-
-    let finInput = ""
+    finance__backspace.addEventListener('click', () => clearLast())
+    finance__clear.addEventListener('click', () => financeClear())
+    let finInput = "", countAfterPoint = 0, includesPoint = false
     const form = document.getElementById('conversionForm');
     const result = document.getElementById('result');
-
+    
     function addNmbr(chr){
+        if((chr === '.' && includesPoint) || countAfterPoint === 2) return
+        if(chr === '.' && !finInput) finInput = "0."
+        else 
         finInput += chr
         amountDisplay.innerHTML = finInput
+        if(includesPoint) countAfterPoint++
+        if(chr === '.') includesPoint = true
     }
-
+    function clearLast(){
+        if(includesPoint && finInput[finInput.length - 1] !== '.') countAfterPoint--
+        else if (finInput[finInput.length - 1] === '.') includesPoint = false
+        finInput = finInput.slice(0, -1)
+        amountDisplay.innerHTML = finInput
+    }
+    function financeClear(){
+        finInput = ""
+        amountDisplay.innerHTML = finInput
+        countAfterPoint = 0
+        includesPoint = false
+    }
     
     async function getExchangeRate() {
         try {
-            const response = await fetch('https://v6.exchangerate-api.com/v6/56f9dd5997ec6a47b0fd64fa/latest/USD');
+            const response = await fetch('https://v6.exchangerate-api.com/v6/56f9dd5997ec6a47b0fd64fa/latest/BYN');
             const data = await response.json();
             return data.conversion_rates.BYN;
         } catch (error) {
