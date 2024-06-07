5c6708100f254ed08a0f969908d135c07451499a
kovaaalex <kovalenko.alex04@gmail.com>
2024-06-07 22:28:12 +0300

    beforePickStart

--- addValutes.js
+++ addValutes.js
@@ -1,4 +1,7 @@
-const from = document.getElementById('from')
+const valute = document.querySelectorAll('.valute')
+const choose__valute = document.querySelectorAll('.choose__valute')
+const from = document.querySelector('#from')
+const to = document.querySelector('#to')
 const url = 'countries.json'
 
 fetch(url)
@@ -6,13 +9,30 @@ fetch(url)
     .then(data => callFunction(data))
 .catch(error => console.error('Ошибка:', error))
 
-function callFunction(data){
+function callFunction(data) {
     const countries = data.countries
-    countries.forEach(country => 
-        from.innerHTML +=
-            `<div class="country">
-                <img src="${country.flag}" alt="Flag of ${country.name}">
-                <div>${country.name}</div>
-                <div>${country.currency}</div>
-            </div>`)
+    valute.forEach(element => {fillElement(element, countries, data)})
+    choose__valute.forEach(choose__element => {fillStartElement(choose__element, countries, data)})
+}
+
+function fillElement(element, countries, data) {
+
+    countries.forEach(country => {
+        element.innerHTML += `
+            <div class="country">
+                <img class="flag" src="${country.flag}" alt="Flag of ${country.name}">
+                <div class="coutry__currency">${country.currency}</div>
+            </div>
+        `
+    })
+}
+
+function fillStartElement(element, countries, data) {
+    const usdIndex = data.countries.findIndex(country => country.currency === 'USD');
+    element.innerHTML += `
+    <div class="country">
+        <img class="flag" src="${countries[usdIndex].flag}" alt="Flag of ${countries[usdIndex].flag}">
+        <div class="coutry__currency">${countries[usdIndex].currency}</div>
+    </div>
+    `
 }
