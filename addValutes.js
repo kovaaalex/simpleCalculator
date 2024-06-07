9a10952642a600d88eaf3915a0466f3ae2a54f00
kovaaalex <kovalenko.alex04@gmail.com>
2024-06-07 18:51:25 +0300

    addCountries

--- addValutes.js
+++ addValutes.js
@@ -1,5 +1,18 @@
 const from = document.getElementById('from')
-from.innerHTML +=
-`<div class="country">
-    Spain
-</div>`
+const url = 'countries.json'
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => callFunction(data))
+.catch(error => console.error('Ошибка:', error))
+
+function callFunction(data){
+    const countries = data.countries
+    countries.forEach(country => 
+        from.innerHTML +=
+            `<div class="country">
+                <img src="${country.flag}" alt="Flag of ${country.name}">
+                <div>${country.name}</div>
+                <div>${country.currency}</div>
+            </div>`)
+}
