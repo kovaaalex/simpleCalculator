0c72bb7a13d296269c5000221c72404f51bfd992
kovaaalex <kovalenko.alex04@gmail.com>
2024-06-08 15:02:43 +0300

    calendarOfCurrentMonth

--- addDate.js
+++ addDate.js
@@ -1,35 +1,44 @@
-const calendars = document.querySelectorAll('.choose__date')
-const month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
-/*const daysInWeek = ['']*/
-calendars.forEach(calendar => addCalendar(calendar))
+const calendars = document.querySelectorAll('.choose__date');
+const month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+const daysOfWeek = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+
+calendars.forEach(calendar => addCalendar(calendar));
 
 function addCalendar(calendar){
     const today = new Date()
     let mnth = today.getMonth()
     let year = today.getFullYear()
     let firstDayOfMonth = new Date(year, mnth, 1).getDay()
-    let lastDayOfMonth = new Date(year, mnth+ 1, 0).getDate()
-    let dayOfWeek = firstDayOfMonth === 0 ? 6 : firstDayOfMonth - 1; // Adjust to start week on Monday
-    calendar.innerHTML += 
+    let lastDateOfMonth = new Date(year, mnth + 1, 0).getDate()
+    let lastDateOfPreviousMonth = new Date(year, mnth, 0).getDate()
+    let dayOfWeek = firstDayOfMonth === 0 ? 6 : firstDayOfMonth - 1
+    let calendarHTML = 
     `<table>
-        <th colspan="7">${month[mnth]}</th>
-        <tr>
-            <td>Пн</td>
-            <td>Вт</td>
-            <td>Ср</td>
-            <td>Чт</td>
-            <td>Пт</td>
-            <td>Сб</td>
-            <td>Вс</td>
-        </tr>
-
-    </table>`
-
-    for (let i = 0; i < dayOfWeek; i++) {
-        calendarHTML += `<td></td>`;
+        <thead>
+            <tr>
+                <th colspan="7">${month[mnth]} ${year}</th>
+            </tr>
+            <tr>`
+    daysOfWeek.forEach(day => {
+        calendarHTML += `<th>${day}</th>`
+    })
+    calendarHTML += `</tr>
+        </thead>
+        <tbody><tr>`
+    for (let i = 0; i < dayOfWeek; i++)
+        calendarHTML += `<td>${lastDateOfPreviousMonth - dayOfWeek + i + 1}</td>`
+    for (let day = 1; day <= lastDateOfMonth; day++) {
+        if (dayOfWeek > 6) {
+            dayOfWeek = 0;
+            calendarHTML += `</tr><tr>`
+        }
+        calendarHTML += `<td>${day}</td>`
+        dayOfWeek++
     }
-}
-
-function addddd(){
-
+    while (dayOfWeek <= 6) {
+        calendarHTML += `<td></td>`
+        dayOfWeek++
+    }
+    calendarHTML += `</tr></tbody></table>`;
+    calendar.innerHTML = calendarHTML;
 }
