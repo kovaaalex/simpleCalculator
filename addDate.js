d730ec0d88bf51f6e4baa5a523cf7858a6b488c0
kovaaalex <kovalenko.alex04@gmail.com>
2024-06-08 23:57:10 +0300

    chooseDate

--- addDate.js
+++ addDate.js
@@ -1,14 +1,18 @@
 const calendars = document.querySelectorAll('.choose__date')
+const chosen__dates = document.querySelectorAll('.selected__date')
+
 const month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
 const daysOfWeek = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
-
+const today = new Date()
+let mnth = today.getMonth()
+let year = today.getFullYear()
 calendars.forEach(calendar => addCalendar(calendar))
 
+chosen__dates.forEach(chosen__date => changeDate(chosen__date))
+function changeDate(chosen__date, ch_dt = today){
+    chosen__date.innerHTML = `${ch_dt.getDate()} ${month[mnth]} ${ch_dt.getFullYear()}`
+}
 function addCalendar(calendar){
-    const today = new Date()
-    let mnth = today.getMonth()
-    let year = today.getFullYear()
-
     function renderCalendar(){
         let firstDayOfMonth = new Date(year, mnth, 1).getDay()
         let lastDateOfMonth = new Date(year, mnth + 1, 0).getDate()
@@ -52,6 +56,7 @@ function addCalendar(calendar){
                 mnth = 11
                 year--
             }
+            addTdListeners()
             renderCalendar()
         })
         calendar.querySelector('.next__month').addEventListener('click', () => {
@@ -60,13 +65,26 @@ function addCalendar(calendar){
                 mnth = 0
                 year++
             }
+            addTdListeners()
             renderCalendar()
         })
+        addTdListeners()
     }
     renderCalendar()
 }
-const tds = document.querySelectorAll('td')
-tds.forEach(td => td.addEventListener('mouseenter', () => {
-    if(td.innerHTML === "") td.style.backgroundColor = "#444546"
-    td.style.cursor = "pointer"
-}))
+
+function addTdListeners(){
+    const tds = document.querySelectorAll('td')
+    tds.forEach(td => td.addEventListener('mouseenter', () => {
+        if(!(+td.innerHTML)) {td.style.backgroundColor = "#444546"
+        td.style.cursor = "pointer"}
+    }))
+    tds.forEach(td => td.addEventListener('click', () => {
+        if(!isNaN(td.innerHTML) && td.innerHTML !== '') {
+            let closestCalendar = td.closest('.choose__date')
+            let inn = closestCalendar.querySelector('.calendar__header span')
+            let text = inn.innerHTML
+            alert(`Выбрана дата: ${td.innerHTML} ${text}`)
+        }
+    }))
+}
