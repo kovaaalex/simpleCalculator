845ff6f9ea851a5077e5b20f9ac299ac46c92d4d
kovaaalex <kovalenko.alex04@gmail.com>
2024-06-09 23:47:44 +0300

    addDateManagementClass

--- addDateWithOOP.js
+++ addDateWithOOP.js
@@ -45,15 +45,74 @@ class Calendar{
         }
         calendarHTML += `</tr></tbody></table>`
         this.element.innerHTML = calendarHTML
-        this.element.querySelector('.prev__month').addEventListener('click', () => {
-            mnth--
-            if (mnth < 0) {
-                mnth = 11
-                year--
-            }
-            renderCalendar()
-        })
+        this.element.querySelector('.prev__month').addEventListener('click', this.changeMonth(-1))
+        this.element.querySelector('.next__month').addEventListener('click', this.changeMonth(1))
         this.element.addTdListeners()
+    }
+    changeMonth(direction){
+        this.mnth += direction
+        if (this.mnth < 0) {
+            this.mnth = 11
+            this.year--
+        }
+        else if (this.mnth > 11) {
+            this.mnth = 0
+            this.year++
+        }
+        this.renderCalendar()
+    }
+    addTdListeners(){
+        const tds = document.querySelectorAll('td')
+        tds.forEach(td => td.addEventListener('mouseenter', () => {
+            if(!(+td.innerHTML)) {
+                td.style.backgroundColor = "#444546"
+                td.style.cursor = "pointer"
+            }
+        }))
+        tds.forEach(td => td.addEventListener('click', () => {
+            if(!isNaN(td.innerHTML) && td.innerHTML !== '') {
+                let closestCalendar = td.closest('.calendar')
+                let closestDT = closestCalendar.closest('.dt')
+                let inn = closestCalendar.querySelector('span')
+                let text = `${td.innerHTML} ${inn.innerHTML}`
+                closestCalendar.querySelector('.selected__date').innerHTML = text
+                if(closestDT.id === 'add__day') 
+                    DateManagement.addDay()
+                else
+                    DateManagement.countDifference()
+            }
+        }))
+    }
+}
+class DateManagement{
+    static convertDate(stringDate){
+        const parts = stringDate.split(" ")
+        const d = +parts[0]
+        const m = month.indexOf(parts[1])
+        const y = +parts[2]
+        const date = new Date(y, m, d)
+        return date
+    }
+    static changeDate(chosen__date, ch_dt = today){
+        chosen__date.innerHTML = `${ch_dt.getDate()} ${month[ch_dt.getMonth()]} ${ch_dt.getFullYear()}`
+    }
+    static addDay(){
+        const mainDate = document.querySelector('#add__day .selected__date')
+        const convertedDate = this.convertDate(mainDate.innerHTML)
+        let addedYear = parseInt(document.querySelector('#year .added__text').innerHTML)
+        let addedMonth = parseInt(document.querySelector('#month .added__text').innerHTML)
+        let addedDay = parseInt(document.querySelector('#day .added__text').innerHTML)
+        if(document.querySelector('#add').checked){
+            convertedDate.setFullYear(convertedDate.getFullYear() + addedYear)
+            convertedDate.setMonth(convertedDate.getMonth() + addedMonth)
+            convertedDate.setDate(convertedDate.getDate() + addedDay)
+        }
+        else{
+            convertedDate.setFullYear(convertedDate.getFullYear() - addedYear)
+            convertedDate.setMonth(convertedDate.getMonth() - addedMonth)
+            convertedDate.setDate(convertedDate.getDate() - addedDay)
+        }
+        this.changeDate(document.querySelector('.added__result'), convertedDate)
     }
 }
 document.addEventListener('DOMContentLoaded', () =>{
